f72fa29c480c7e908ce39f90b8f3516177ac7080
Vishnu <vishnusankarakumar@gmail.com>
2025-10-07 17:01:11 +0100

    remove all old architecture server files

--- server/src/util.ts
+++
@@ -1,4 +0,0 @@
-import crypto from 'crypto';
-export const hash = (s: string) => crypto.createHash('sha256').update(s).digest('hex');
-export const sig = (f: Record<string, any>) => hash(JSON.stringify(f));
-export function tryParseJSON<T=any>(s: string): T | null { try { return JSON.parse(s) as T; } catch { return null; } }
